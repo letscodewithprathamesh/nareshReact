290543f7c0de36fd7b31e0a11bd80c877d49c316
Prathamesh <prathameshyadav100@gmail.com>
2024-03-24 17:29:51 +0530

    Done

--- bg_changer/src/ColourSelector.jsx
+++ bg_changer/src/ColourSelector.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
+
 function ColourSelector()
 {
+    const[bgcolor,SetBgcolor]=useState('#000000')
+
+ const select=(event)=>{
+    SetBgcolor(event.target.value)
+    console.log(event.target); 
+    console.log(event.target.value);   
+    /*the event is send as an object by eventListner (Onchange is HTML event) , the object contains {<input type="color" value="#ad4848">} to select value we use event.target.value (which gives hexadecimal code)
+    which we set to SetBgcolor function which changes the state of bgcolor whenever we pick color from Color picker
+     */
+ }
+
+
     return(
-        <div className="selectors">
-            <input type="color" />
+        <div className="selectors" style={{backgroundColor:bgcolor}}>
+            <input type="color" onChange={select} value={bgcolor}/>
 
         </div>
     )
--- bg_changer/src/main.jsx
+++ bg_changer/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AppClass from './AppClass.jsx'
+import ColourSelector from './ColourSelector.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
